91f06e8c0d37fd80960d0a294e9f33ce9ac908cc
Namjun Kim <namjunemy@gmail.com>
2018-12-11 17:48:13 +0900

    커스텀 Executor로 CompletableFuture 활용 최적화하기(스레드 수 최적화)

--- src/ShopApplication.java
+++ src/ShopApplication.java
@@ -3,10 +3,24 @@ import static java.util.stream.Collectors.toList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadFactory;
 
 public class ShopApplication {
 
-    private List<Shop> shops = Arrays.asList(
+    private final Executor executor =
+        // 상점 수만큼의 스레드를 갖는 풀을 생성한다. 범위는 0~100
+        Executors.newFixedThreadPool(Math.min(shops.size(), 100), new ThreadFactory() {
+            @Override
+            public Thread newThread(Runnable r) {
+                Thread t = new Thread(r);
+                t.setDaemon(true);  //프로그램 종료를 방해하지 않는 데몬 스레드를 사용한다.
+                return t;
+            }
+        });
+
+    private static List<Shop> shops = Arrays.asList(
         new Shop("apple"),
         new Shop("samsung"),
         new Shop("nokia"),
@@ -28,7 +42,8 @@ public class ShopApplication {
         List<CompletableFuture<String>> priceFutures =
             shops.stream()
                 .map(shop -> CompletableFuture
-                    .supplyAsync(() -> shop.getName() + "price is " + shop.calculatePrice(product)))
+                    .supplyAsync(() -> shop.getName() + "price is " + shop.calculatePrice(product),
+                        executor))
                 .collect(toList());
 
         return priceFutures.stream()

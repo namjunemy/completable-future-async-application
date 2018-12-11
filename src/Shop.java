d78e1cc07f24f9efe57423231a177236d58dba64
Namjun Kim <namjunemy@gmail.com>
2018-12-11 14:34:12 +0900

    동기 메서드를 비동기 메서드로 변환

--- src/Shop.java
+++ src/Shop.java
@@ -1,17 +1,27 @@
 import java.util.Random;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.Future;
 
 public class Shop {
 
     private static Random random = new Random();
 
     /**
-     * 제품명에 해당하는 가격을 반환
+     * 제품명에 해당하는 가격을 비동기로 반환
      *
      * @param product the product name
      * @return double price
      */
-    public double getPrice(String product) {
-        return calculatePrice(product);
+    public Future<Double> getPriceAsync(String product) {
+        //계산 결과를 포함할 CompletableFuture를 생성
+        CompletableFuture<Double> futurePrice = new CompletableFuture<>();
+
+        new Thread(() -> {
+            double price = calculatePrice(product); //다른 스레드에서 비동기적으로 계산을 수행
+            futurePrice.complete(price);            //오랜 시간이 걸리는 계산이 완료되면 Future에 값을 설정
+        }).start();
+
+        return futurePrice; //계산 결과가 완료되길 기다리지 않고 Future를 반환한다.
     }
 
     /**
--- src/ShopApplication.java
+++ src/ShopApplication.java
@@ -1,3 +1,5 @@
+import java.util.concurrent.Future;
+
 public class ShopApplication {
 
     public static void main(String[] args) {
@@ -5,12 +7,23 @@ public class ShopApplication {
 
         //Start
         long start = System.nanoTime();
-
-        double price = shop.getPrice("nike");
-
+        Future<Double> futurePrice = shop.getPriceAsync("nike");
         long invocationTime = ((System.nanoTime() - start) / 1_000_000);
+        System.out.println("호출이 리턴되는 시간: " + invocationTime + "msecs");
 
-        System.out.println("수행시간: " + invocationTime);
-        System.out.println("price: " + price);
+        doSomethingElse();
+        try {
+            double price = futurePrice.get(); //가격 정보가 있으면 Future에서 가격 정보를 읽고, 없으면 받을 때까지 블록한다.
+            System.out.println("price: " + price);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+        long retrievalTime = ((System.nanoTime() - start) / 1_000_000);
+        System.out.println("가격이 리턴되는 시간: " + retrievalTime + "msecs");
+    }
+
+    private static void doSomethingElse() {
+        System.out.println("제품의 가격을 계산하는 동안 다른 작업을 수행중...");
     }
 }

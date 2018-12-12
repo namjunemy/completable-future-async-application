2e4e4270341f1e53bedd2589907ab59e6e6a8d0c
Namjun Kim <namjunemy@gmail.com>
2018-12-12 11:24:26 +0900

    동기방식의 할인 가격 계산 로직을 CompletableFuture API 비동기 연산 파이프라인으로 처리하기(성능개선)

--- src/ShopApplication.java
+++ src/ShopApplication.java
@@ -2,10 +2,24 @@ import static java.util.stream.Collectors.toList;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadFactory;
 
 public class ShopApplication {
 
-    private List<Shop> shops = Arrays.asList(
+    private final Executor executor =
+        Executors.newFixedThreadPool(Math.min(shops.size(), 100), new ThreadFactory() {
+            @Override
+            public Thread newThread(Runnable r) {
+                Thread t = new Thread(r);
+                t.setDaemon(true);
+                return t;
+            }
+        });
+
+    private static List<Shop> shops = Arrays.asList(
         new Shop("apple"),
         new Shop("samsung"),
         new Shop("nokia"),
@@ -24,10 +38,22 @@ public class ShopApplication {
     }
 
     public List<String> findPrices(String product) {
-        return shops.stream()
-            .map(shop -> shop.getPrice(product))
-            .map(Quote::parse)
-            .map(Discount::applyDiscount)
+        List<CompletableFuture<String>> priceFutures =
+            shops.stream()
+                //각 상점에서 할인전 가격을 비동기적으로 얻는다. return Stream<CompletableFuture<String>>
+                .map(shop -> CompletableFuture.supplyAsync(
+                    () -> shop.getPrice(product), executor))
+
+                // 상점에서 반환한 문자열을 Quote 객체로 변환한다. return Stream<CompletableFuture<Quote>>
+                .map(future -> future.thenApply(Quote::parse))
+
+                //결과 Future를 다른 비동기 작업과 조합해서 할인 코드를 적용한다. return Stream<CompletableFuture<String>>
+                .map(future -> future.thenCompose(quote -> CompletableFuture.supplyAsync(
+                    () -> Discount.applyDiscount(quote), executor)))
+                .collect(toList());
+
+        return priceFutures.stream()
+            .map(CompletableFuture::join)   // 스트림의 모든 Future가 종료되길 기다렸다가 각각의 결과를 추출한다.
             .collect(toList());
     }
 }
